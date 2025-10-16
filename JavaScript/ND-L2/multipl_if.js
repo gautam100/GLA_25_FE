5f1f3ccee38c90c15c37960d8e9e6e414339732d
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-10-16 15:13:04 +0530

    Multiple if

--- JavaScript/ND-L2/multipl_if.js
+++ JavaScript/ND-L2/multipl_if.js
@@ -1,26 +1,27 @@
-function calculateTotal() { //debugger;
+function calculateTotal() { 
+    //debugger;
   let eng=0,math=0, sci = 0, total=0, percentage =0, grade="";
 
   eng = parseInt(document.getElementById("sub1").value);
   math = parseInt(document.getElementById("sub2").value);
   sci = parseInt(document.getElementById("sub3").value);
 
-    // if(eng === ""){
-    //     eng=0;
-    // }
-    // if(math === ""){
-    //     math=0;
-    // }
-    // if(sci == ""){
-    //     sci=0;
-    // }
+    if(isNaN(eng)){
+        eng=0;
+    }
+    if(isNaN(math)){
+        math=0;
+    }
+    if(isNaN(sci)){
+        sci=0;
+    }
     
   
     total = eng + math + sci;
     percentage = total/300 *100
 
     document.getElementById("total_marks").innerHTML = "Total:"+total;
-    document.getElementById("percentage").innerHTML = "Percentage:"+percentage;
+    document.getElementById("percentage").innerHTML = "Percentage:"+percentage.toFixed(2);
 
     if(percentage >=75){
         grade = "A"
