6a776c7a958be9e9615057f93624105847833ae6
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-10-17 10:34:58 +0530

    Conditional statements

---
+++ JavaScript/ND-L1/9_multiple_if.js
@@ -0,0 +1,33 @@
+function generateReportCard(){
+    //debugger;
+    let eng = parseInt(document.getElementById("sub1").value)
+    let math = parseInt(document.getElementById("sub2").value)
+    let sci = parseInt(document.getElementById("sub3").value)
+
+    let total = 0, percentage=0, grade=undefined;
+    if(isNaN(eng)){
+        eng = 0
+    }
+    if(isNaN(math)){
+        math = 0
+    }
+    if(isNaN(sci)){
+        sci = 0
+    }
+    total = eng + math + sci
+    percentage = total/300*100
+    document.getElementById("total").innerHTML = "<b>Totak Marks:"+total+"</b>" 
+    document.getElementById("percentage").innerHTML = "<b>Percentage:"+percentage.toFixed(2)+"</b>" 
+    if(percentage>=75){
+        grade= "A"
+    }else if(percentage>=60 && percentage<75){
+        grade= "B"
+    }else if(percentage>=45 && percentage<60){
+        grade = "C"
+    }else{
+        grade= "Fail"
+    }
+    document.getElementById("grade").innerHTML = "<b>Grade:"+grade+"</b>" 
+
+
+}
