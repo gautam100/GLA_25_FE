f13adebb183cf6f9f2743e51463e612241f13c1c
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-11-06 10:00:58 +0530

    Changes in validation code

--- JavaScript/Revision/ND/3_validation.js
+++ JavaScript/Revision/ND/3_validation.js
@@ -1,12 +1,15 @@
-let validation = function(){
-    if(document.querySelector("#first_name").value === ""){
-        //alert("Enter First name!")
-        document.querySelector("#first_name_err").innerHTML = "Please Enter First Name!"
-        document.querySelector("#first_name").style.border = "2px solid red"
-        return false;
-    }
-    else if(document.querySelector("#last_name").value === ""){
-        alert("Enter Last name!")
-        return false;
-    }
-}
+let validation = function () {
+  if (document.querySelector("#first_name").value === "") {
+    //alert("Enter First name!")
+    document.querySelector("#first_name_err").innerHTML =
+      "Please Enter First Name!";
+    document.querySelector("#first_name").style.border = "2px solid red";
+    return false;
+  } else if (document.querySelector("#last_name").value === "") {
+    // alert("Enter Last name!")
+    document.querySelector("#last_name_err").innerHTML =
+      "Please Enter Last Name!";
+    document.querySelector("#last_name").style.border = "2px solid red";
+    return false;
+  }
+};
