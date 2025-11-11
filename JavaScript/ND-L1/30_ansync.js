fb07e841f22a04c713a27ae822fdbc05246fa311
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-11-11 11:24:16 +0530

    call back

---
+++ JavaScript/ND-L1/30_ansync.js
@@ -0,0 +1,21 @@
+/*
+- synchronous 
+- **Asynchronous
+
+1) callback
+2) promise
+3) async/await
+
+*/
+
+function Xyz() {
+  console.log("First Line");
+
+  setTimeout(function () {
+    console.log("Second Line");
+  }, 1000);
+
+  console.log("Third Line");
+}
+
+Xyz();
